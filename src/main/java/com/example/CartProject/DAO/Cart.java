1b29f748d297d19c009e4bacf3073ab55ba31a80
Juan Francsico Carrión valverde <juanfrancv10@gmail.com>
2025-03-14 12:06:41 +0100

    clean

--- src/main/java/com/example/CartProject/DAO/Cart.java
+++ src/main/java/com/example/CartProject/DAO/Cart.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static java.util.UUID.randomUUID;
-
 public class Cart {
 
 
--- src/test/java/com/example/CartProject/controllers/CartController2.java
+++
@@ -1,8 +0,0 @@
-package com.example.CartProject.controllers;
-
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CartController2 {
-}
